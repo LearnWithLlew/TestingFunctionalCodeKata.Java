30baa78cba0ed63fe17da2a8c39031baa9002e2e
Llewellyn Falco <isidore@setgame.com>
2017-10-24 22:58:22 -0700

    added custom assert

--- src/test/java/org/learnwithllew/testingkatas/p01/TrigMathTest.java
+++ src/test/java/org/learnwithllew/testingkatas/p01/TrigMathTest.java
@@ -1,5 +1,6 @@
 package org.learnwithllew.testingkatas.p01;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 import org.junit.Test;
@@ -9,11 +10,15 @@ import org.junit.Test;
  * Tools: EclEmma (Test Coverage)
  *        ApprovalTests (verification package)
  */
-public class TrigMathTest {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-
+public class TrigMathTest
+{
+  @Test
+  public void test()
+  {
+    fail("Not yet implemented");
+  }
+  private void assertDouble(double expected, double actual)
+  {
+    assertEquals(expected + "", actual + "");
+  }
 }
